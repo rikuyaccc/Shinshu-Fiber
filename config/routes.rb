a6b2db1268b62a445fb830fb894762b9f85a07a1
千葉陸矢 <chibarikuya@chibarikuyanoMacBook-Air.local>
2020-12-02 18:40:00 +0900

    ユーザー詳細viewの修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
   get 'home/myself'
+  get 'home/terms'
+  get 'home/privacy'
 
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
